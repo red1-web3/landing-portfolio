c1a80fc680c41ade2a206401d023a61ee90959e6
red1-web3 <red1.web3@gmail.com>
2023-08-26 20:50:24 +0600

    Hero text change

--- src/views/Home/components/HeroSection/index.tsx
+++ src/views/Home/components/HeroSection/index.tsx
@@ -7,18 +7,17 @@ const HeroSection = () => {
         <div className="grid md:grid-cols-[3fr,2fr] md:gap-x-5 items-center gap-y-6">
           <div className="max-md:order-2">
             <h4 className="text-xl lg:text-[32px]/[33px] ml-1.5 tracking-[0.18px] font-extrabold">
-              Hi, I’m a
+              Hi, I’m
             </h4>
             <h1 className="text-4xl lg:text-[80px]/[103%] __gradient1 lg:mt-2 tracking-[0.18px] font-extrabold">
-              Senior Backend Engineer
+              Pankaj Singh Solanki
             </h1>
 
             <p className="my-4 lg:my-[30px] max-w-[595px] md:text-lg lg:text-xl">
-              I’m <strong>Pankaj Singh Solanki</strong>, and a Software
-              Developer with a specialization in <strong>Backend</strong> &{" "}
-              <strong>DevOps</strong>,{" "}
+              I’m a <strong>Software Developer</strong> with a specialization in{" "}
+              <strong>Backend</strong> & <strong>DevOps</strong>,{" "}
               <strong>Infrastructure Management</strong> ,{" "}
-              <strong>Solution Architecture</strong>
+              <strong>Solution Architecture </strong>
               and <strong>Structural Analysis</strong>.
             </p>
             <button className="border-2 border-transparent hover:border-accent hover:bg-transparent hover:text-accent duration-300 px-4 h-[46px] rounded bg-accent text-white font-medium __c_all">
