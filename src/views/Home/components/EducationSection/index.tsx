9543851b4e52b833b2969ff09896c155ae8fcaf7
red1-web3 <red1.web3@gmail.com>
2023-08-26 19:05:08 +0600

    Responsive

--- src/views/Home/components/EducationSection/index.tsx
+++ src/views/Home/components/EducationSection/index.tsx
@@ -8,8 +8,8 @@ const EducationSection = () => {
       <div className="__container">
         <SectionTitle title="Education" />
         <div className="mt-8 lg:mt-16">
-          <div className="grid grid-cols-[60%,40%] gap-8 w-full">
-            <div className="bg-lime-200/40 rounded-lg p-9">
+          <div className="grid lg:grid-cols-[60%,40%] gap-5 lg:gap-8 w-full">
+            <div className="bg-lime-200/40 rounded-lg p-7 md:p-9">
               <h2 className="text-xl md:text-2xl lg:text-3xl font-bold">
                 Bachelor Of Technology (Computer Science)
               </h2>
@@ -20,23 +20,31 @@ const EducationSection = () => {
                 Rajasthan Technical University
               </a>
 
-              <ul className="py-10 space-y-3">
+              <ul className="pt-10 space-y-3">
                 <li className="flex items-center gap-x-3">
-                  <CheckIcon />
+                  <div className="shrink-0">
+                    <CheckIcon />
+                  </div>
                   <p>Bachelors degree focused on Cloud Computing</p>
                 </li>
                 <li className="flex items-center gap-x-3">
-                  <CheckIcon />
+                  <div className="shrink-0">
+                    <CheckIcon />
+                  </div>
                   <p>Data Structures</p>
                 </li>
                 <li className="flex items-center gap-x-3">
-                  <CheckIcon />
+                  <div className="shrink-0">
+                    <CheckIcon />
+                  </div>
                   <p>
                     Structures, Algorithms, Software Development Life Cycle,
                   </p>
                 </li>
                 <li className="flex items-center gap-x-3">
-                  <CheckIcon />
+                  <div className="shrink-0">
+                    <CheckIcon />
+                  </div>
                   <p>
                     Big Data Analytics with a <strong>9.5 CGPA</strong>
                   </p>
